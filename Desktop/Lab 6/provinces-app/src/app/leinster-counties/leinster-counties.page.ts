6750f700298dc0e924df02241762939cbc904540
Husayn / Fionn <124906203+HusaynPrograms@users.noreply.github.com>
2025-03-17 12:54:01 +0000

    Update leinster-counties.page.ts
    Comments

--- Desktop/Lab 6/provinces-app/src/app/leinster-counties/leinster-counties.page.ts
+++ Desktop/Lab 6/provinces-app/src/app/leinster-counties/leinster-counties.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+//Ionic UI Components
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
 
 @Component({
@@ -11,13 +12,14 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton 
   imports: [IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class LeinsterCountiesPage implements OnInit {
-
+//Array storing Counties Names
   counties: string[] = ["Carlow", "Dublin", "Kildare",
     "Kilkenny", "Laois", "Longford", "Louth", "Meath",
     "Offaly", "Westmeath", "Wexford", "Wicklow"];
 
   constructor() { }
 
+  //Runs when page loads 
   ngOnInit() {
   }
 
